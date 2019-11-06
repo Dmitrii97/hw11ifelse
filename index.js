d286121962a4b2cf8cc1cd498b67470e63c8714c
Dima <hotabic3@gmail.com>
2019-11-06 12:12:12 +0200

    hw11ifelse

--- index.js
+++ index.js
@@ -2,25 +2,10 @@ var weekDayIf = prompt('Please enter Day');
 var weekDayIfUpper = weekDayIf.toUpperCase();
 
 
-if (weekDayIfUpper == 'MONDAY') {
+if (weekDayIfUpper == 'MONDAY' || 'TUEDSAY' || 'WEDNESDAY' || 'THURSDAY' || 'FRIDAY') {
     weekDayResult = 'Working day';
 } else
-if (weekDayIfUpper == 'TUEDSAY') {
-    weekDayResult = 'Working day';
-} else
-if (weekDayIfUpper == 'WEDNESDAY') {
-    weekDayResult = 'Working day';
-} else
-if (weekDayIfUpper == 'THURSDAY') {
-    weekDayResult = 'Working day';
-} else
-if (weekDayIfUpper == 'FRIDAY') {
-    weekDayResult = 'Working day';
-} else
-if (weekDayIfUpper == 'SATURDAY') {
-    weekDayResult = 'Weekend';
-} else
-if (weekDayIfUpper == 'SUNDAY') {
+if (weekDayIfUpper == 'SATURDAY' || 'SUNDAY') {
     weekDayResult = 'Weekend';
 } else {
     weekDayResult = 'Can not define, please try another value';
